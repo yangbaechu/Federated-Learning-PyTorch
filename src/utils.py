ef11086441c6b7419b06f3023152d10c6766fb07
yangbaechu <abab_1212@naver.com>
2023-01-15 20:21:51 +0900

    데이터 분포 확인, FedAVG, FedSGD test

--- src/utils.py
+++ src/utils.py
@@ -76,7 +76,8 @@ def average_weights(w):
     Returns the average of the weights.
     """
     w_avg = copy.deepcopy(w[0])
-    for key in w_avg.keys():
+    print(w_avg.keys())
+    for key in w_avg.keys(): #각 LAYER에 대해 실행
         for i in range(1, len(w)):
             w_avg[key] += w[i][key]
         w_avg[key] = torch.div(w_avg[key], len(w))
